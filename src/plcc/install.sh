7f8a0ae5facb94122a5a1535a9ab5768e893894d
Stoney Jackson <dr.stoney@gmail.com>
2025-10-24 19:52:48 +0000

    fix: mcr.microsoft.com/devcontainers/base:ubuntumcr.microsoft.com/devcontainers/base:ubuntu

--- src/plcc/install.sh
+++ src/plcc/install.sh
@@ -2,4 +2,5 @@
 set -e
 
 echo "Activating feature 'plcc'"
-/bin/bash -c "$(curl -fsSL https://github.com/ourPLCC/plcc/raw/main/installers/plcc/install.bash)" >> ${_CONTAINER_USER_HOME}/.bashrc
+echo "_CONTAINER_USER_HOME=${_CONTAINER_USER_HOME}"
+/bin/bash -c "$(curl -fsSL https://github.com/ourPLCC/plcc/raw/main/installers/plcc/install.bash)" >> "${_CONTAINER_USER_HOME}/.bashrc"
